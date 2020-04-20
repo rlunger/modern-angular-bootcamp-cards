7c5ed1378fbbac6a0eb7295c9e3caa6cb335f5ab
Ryan Lunger <ryanlunger@gmail.com>
2020-04-19 19:17:08 -0500

    WIP implementation of a basic card component in Angular/TypeScript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'modern-angular-bootcamp-cards';
+  posts = [
+    {
+      title: 'Nature Deemed Non-Toxic, Millenials Remain Indoors Nonetheless',
+      subtitle: 'A real news story',
+      image: 'assets/card-image-1.jpg',
+      text: "Of on affixed civilly moments promise explain fertile in. Assurance advantage belonging happiness departure so of. Now improving and one sincerity intention allowance commanded not. Oh an am frankness be necessary earnestly advantage estimable extensive. Five he wife gone ye. Mrs suffering sportsmen earnestly any. In am do giving to afford parish settle easily garret."
+    },
+    {
+      title: 'Man Gets Up Early, Becomes King of the World',
+      subtitle: 'A real news story',
+      image: 'assets/card-image-2.jpg',
+      text: "Of on affixed civilly moments promise explain fertile in. Assurance advantage belonging happiness departure so of. Now improving and one sincerity intention allowance commanded not. Oh an am frankness be necessary earnestly advantage estimable extensive. Five he wife gone ye. Mrs suffering sportsmen earnestly any. In am do giving to afford parish settle easily garret."
+    },
+    {
+      title: 'Scientists Prove Moutains are Jurassic Anthills',
+      subtitle: 'A real news story',
+      image: 'assets/card-image-3.jpg',
+      text: "Of on affixed civilly moments promise explain fertile in. Assurance advantage belonging happiness departure so of. Now improving and one sincerity intention allowance commanded not. Oh an am frankness be necessary earnestly advantage estimable extensive. Five he wife gone ye. Mrs suffering sportsmen earnestly any. In am do giving to afford parish settle easily garret."
+    },
+    {
+      title: 'Nature Deemed Non-Toxic, Millenials Remain Indoors Nonetheless',
+      subtitle: 'A real news story',
+      image: 'assets/card-image-1.jpg',
+      text: "Of on affixed civilly moments promise explain fertile in. Assurance advantage belonging happiness departure so of. Now improving and one sincerity intention allowance commanded not. Oh an am frankness be necessary earnestly advantage estimable extensive. Five he wife gone ye. Mrs suffering sportsmen earnestly any. In am do giving to afford parish settle easily garret."
+    },
+    {
+      title: 'Man Gets Up Early, Becomes King of the World',
+      subtitle: 'A real news story',
+      image: 'assets/card-image-2.jpg',
+      text: "Of on affixed civilly moments promise explain fertile in. Assurance advantage belonging happiness departure so of. Now improving and one sincerity intention allowance commanded not. Oh an am frankness be necessary earnestly advantage estimable extensive. Five he wife gone ye. Mrs suffering sportsmen earnestly any. In am do giving to afford parish settle easily garret."
+    },
+    {
+      title: 'Scientists Prove Moutains are Jurassic Anthills',
+      subtitle: 'A real news story',
+      image: 'assets/card-image-3.jpg',
+      text: "Of on affixed civilly moments promise explain fertile in. Assurance advantage belonging happiness departure so of. Now improving and one sincerity intention allowance commanded not. Oh an am frankness be necessary earnestly advantage estimable extensive. Five he wife gone ye. Mrs suffering sportsmen earnestly any. In am do giving to afford parish settle easily garret."
+    },
+    {
+      title: 'Nature Deemed Non-Toxic, Millenials Remain Indoors Nonetheless',
+      subtitle: 'A real news story',
+      image: 'assets/card-image-1.jpg',
+      text: "Of on affixed civilly moments promise explain fertile in. Assurance advantage belonging happiness departure so of. Now improving and one sincerity intention allowance commanded not. Oh an am frankness be necessary earnestly advantage estimable extensive. Five he wife gone ye. Mrs suffering sportsmen earnestly any. In am do giving to afford parish settle easily garret."
+    },
+    {
+      title: 'Man Gets Up Early, Becomes King of the World',
+      subtitle: 'A real news story',
+      image: 'assets/card-image-2.jpg',
+      text: "Of on affixed civilly moments promise explain fertile in. Assurance advantage belonging happiness departure so of. Now improving and one sincerity intention allowance commanded not. Oh an am frankness be necessary earnestly advantage estimable extensive. Five he wife gone ye. Mrs suffering sportsmen earnestly any. In am do giving to afford parish settle easily garret."
+    },
+    {
+      title: 'Scientists Prove Moutains are Jurassic Anthills',
+      subtitle: 'A real news story',
+      image: 'assets/card-image-3.jpg',
+      text: "Of on affixed civilly moments promise explain fertile in. Assurance advantage belonging happiness departure so of. Now improving and one sincerity intention allowance commanded not. Oh an am frankness be necessary earnestly advantage estimable extensive. Five he wife gone ye. Mrs suffering sportsmen earnestly any. In am do giving to afford parish settle easily garret."
+    }
+  ]
 }
